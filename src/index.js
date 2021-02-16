087508b323a2688d549cbdd2eeb5995409a7ea89
unknown <laicheehoong@gmail.com>
2021-02-16 08:24:33 +0800

    created project with create react app and added files for homepage

--- src/index.js
+++ src/index.js
@@ -2,16 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+  <App />
+  ,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/*************************** from teacher ****************************** */
+//git remote add origin https://github.com/LAICHEEHOONG/crwn-clothing-2.git
+//git status
+//git add -A
+//git commit -m ""
+//git push origin master
+
+/*************************** from git hub ****************************** */
+//create a new repository on the command line
+// echo "# crwn-clothing-2" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/LAICHEEHOONG/crwn-clothing-2.git
+// git push -u origin main
+
+/*************************** from git hub ****************************** */
+// git remote add origin https://github.com/LAICHEEHOONG/crwn-clothing-2.git
+// git branch -M main
+// git push -u origin main
