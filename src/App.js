8e959c427dff364a9f9adedb738038cf7d77b699
unknown <laicheehoong@gmail.com>
2021-03-13 07:20:34 +0800

    implemented firebase utils, included ability to store authenticated users into our firestore database

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.compoent';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.utils';
-
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import './App.css';
 
 
@@ -26,10 +25,27 @@ class App extends React.Component {
   componentDidMount() {
     console.log('Component did mount');
 
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
 
-      console.log(user);
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          })
+        });
+
+      }
+
+      this.setState({ currentUser: userAuth }, () => console.log(this.state));
+
+
+
     })
 
   }
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,48 +3,62 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC1pUGftO5JzDbDRir_emPbiNFWtUbKDYE",
-    authDomain: "crwn-db-2-cf6b9.firebaseapp.com",
-    projectId: "crwn-db-2-cf6b9",
-    storageBucket: "crwn-db-2-cf6b9.appspot.com",
-    messagingSenderId: "501901909155",
-    appId: "1:501901909155:web:2f9ee5fd7d05528ea47316",
-    measurementId: "G-DGQSX8P42C"
-  };
+  apiKey: "AIzaSyC1pUGftO5JzDbDRir_emPbiNFWtUbKDYE",
+  authDomain: "crwn-db-2-cf6b9.firebaseapp.com",
+  projectId: "crwn-db-2-cf6b9",
+  storageBucket: "crwn-db-2-cf6b9.appspot.com",
+  messagingSenderId: "501901909155",
+  appId: "1:501901909155:web:2f9ee5fd7d05528ea47316",
+  measurementId: "G-DGQSX8P42C"
+};
 
-  firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
-  export const auth = firebase.auth();
-  export const firestore = firebase.firestore();
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
 
-  const provider = new firebase.auth.GoogleAuthProvider();
+const provider = new firebase.auth.GoogleAuthProvider();
 
-  provider.setCustomParameters({
-    'prompt': 'select_account'
-  });
+provider.setCustomParameters({
+  'prompt': 'select_account'
+});
 
-  export const signInWithGoogle = () => auth.signInWithPopup(provider);
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
-  export default firebase;
+export const createUserProfileDocument = async (userAuth, additionalData) => {
 
-//   firebase.auth()
-//   .signInWithPopup(provider)
-//   .then((result) => {
-//     /** @type {firebase.auth.OAuthCredential} */
-//     var credential = result.credential;
 
-//     // This gives you a Google Access Token. You can use it to access the Google API.
-//     var token = credential.accessToken;
-//     // The signed-in user info.
-//     var user = result.user;
-//     // ...
-//   }).catch((error) => {
-//     // Handle Errors here.
-//     var errorCode = error.code;
-//     var errorMessage = error.message;
-//     // The email of the user's account used.
-//     var email = error.email;
-//     // The firebase.auth.AuthCredential type that was used.
-//     var credential = error.credential;
-//     // ...
-//   });
+
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+
+  }
+
+  return userRef;
+
+}
+
+
+export default firebase;
+
